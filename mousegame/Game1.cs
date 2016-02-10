2c92096522203808fe55482ac402bdd3e6758dd7
matthewkreutter <matthew.kreutter@gmail.com>
2016-02-10 12:16:54 -0600

    Update README.md

--- mousegame/Game1.cs
+++ mousegame/Game1.cs
@@ -73,7 +73,7 @@ namespace mousegame
             Content.RootDirectory = "Content";
             graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
             graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-            graphics.IsFullScreen = true;
+            graphics.IsFullScreen = false;
             graphics.ApplyChanges();
         }
 
